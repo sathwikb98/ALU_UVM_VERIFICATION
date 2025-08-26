73db86db6700900643113592487073185bcb3076
Sathwik Bhat <109812189+sathwikb98@users.noreply.github.com>
2025-08-26 17:56:20 +0530

    Update coverage.sv

--- src/coverage.sv
+++ src/coverage.sv
@@ -1,68 +1,72 @@
-`uvm_analysis_imp_decl(_drv_cg)
-`uvm_analysis_imp_decl(_mon_cg)
+`uvm_analysis_imp_decl(_active_mon_cg)
+`uvm_analysis_imp_decl(_passive_mon_cg)
 
-// coverage or subsrcriber logic used to get the coverage of input [from driver] and output [from monitor].
 class coverage extends uvm_component;
   `uvm_component_utils(coverage)
-  uvm_analysis_imp_mon_cg#(sequence_item, coverage) monitor_cov;
-  uvm_analysis_imp_drv_cg#(sequence_item, coverage) driver_cov;
 
-  sequence_item pktd;
-  sequence_item pktm;
+  // Gets the data from the active/passive monitor !
+  uvm_analysis_imp_active_mon_cg#(sequence_item, coverage) active_mon_cov;
+  uvm_analysis_imp_passive_mon_cg#(sequence_item, coverage) passive_mon_cov;
 
-  covergroup drv_cg;
-    c1: coverpoint pktd.OPA {
+  sequence_item a_pktm;
+  sequence_item p_pktm;
+
+  covergroup inp_cg;
+    c1: coverpoint a_pktm.OPA {
       bins a1 = {[0:255]};
     }
-    c2: coverpoint pktd.OPB {
+    c2: coverpoint a_pktm.OPB {
       bins a2 = {[0:255]};
     }
-    c3: coverpoint pktd.CIN;
-    c4: coverpoint pktd.MODE;
-    c5: coverpoint pktd.INP_VALID;
-    c6: coverpoint pktd.CMD;
+    c3: coverpoint a_pktm.CIN;
+    c4: coverpoint a_pktm.MODE;
+    c5: coverpoint a_pktm.INP_VALID;
+    c6: coverpoint a_pktm.CMD {
+      bins a3 = {[0:13]};
+      ignore_bins bad = {[14:$]};
+    }
     c7: cross c4, c6 ; // cmd and mode cross !
     c8: cross c4, c5 ; // mode and input_valid !
     c9: cross c5, c6 ; // inp_valid and cmd !
   endgroup
 
-  covergroup mon_cg;
-    c1: coverpoint pktm.RES{
-      bins a1 = {0}; // least value
-      bins a2 = {511}; // 9 bit max value
-      bins a3 = default;
+  covergroup out_cg;
+    c1: coverpoint p_pktm.RES; //{
+      //bins a1 = {0}; // least value
+      //bins a2 = {511}; // 9 bit max value
+      //bins a3 = default;
+    //}
+    c2: coverpoint {p_pktm.G,p_pktm.E,p_pktm.L}{
+      bins a1 = {[1:$]};
     }
-    c2: coverpoint pktm.G;
-    c3: coverpoint pktm.E;
-    c4: coverpoint pktm.L;
-    c5: coverpoint pktm.COUT;
-    c6: coverpoint pktm.OFLOW;
-    c7: coverpoint pktm.ERR;
+    c5: coverpoint p_pktm.COUT;
+    c6: coverpoint p_pktm.OFLOW;
+    c7: coverpoint p_pktm.ERR;
   endgroup
 
   function new(string name ="coverage", uvm_component parent = null);
     super.new(name,parent);
 
-    drv_cg = new();
-    mon_cg = new();
-    monitor_cov = new("mon_cov",this);
-    driver_cov = new("drv_cov", this);
+    inp_cg = new();
+    out_cg = new();
+    active_mon_cov = new("active_mon_cov",this);
+    passive_mon_cov = new("passive_mon_cov",this);
   endfunction
 
-  function void write_drv_cg(sequence_item req);
-    pktd = req;
-    drv_cg.sample();
+  function void write_active_mon_cg(sequence_item req);
+    a_pktm = req;
+    inp_cg.sample();
   endfunction
 
-  function void write_mon_cg(sequence_item req);
-    pktm = req;
-    mon_cg.sample();
+  function void write_passive_mon_cg(sequence_item req);
+    p_pktm = req;
+    out_cg.sample();
   endfunction
 
   function void final_phase(uvm_phase phase);
     super.final_phase(phase);
-    // used to print functional coverage of drv_cg and mon_cg !
-    `uvm_info(get_type_name(), $sformatf("COVERAGE, drv_cg: %0.2f, mon_cg: %.2f",drv_cg.get_coverage(),mon_cg.get_coverage()),UVM_LOW)
+
+    `uvm_info(get_type_name(), $sformatf("COVERAGE, inp_cg: %0.2f, out_cg: %.2f",inp_cg.get_coverage(),out_cg.get_coverage()),UVM_LOW)
 
   endfunction
 
