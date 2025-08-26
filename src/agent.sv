e0df8d42db2eafbb505e5b9b2d3e5fc126fe0cd1
Sathwik Bhat <109812189+sathwikb98@users.noreply.github.com>
2025-08-26 17:19:12 +0530

    Update agent.sv

--- src/agent.sv
+++ src/agent.sv
@@ -5,13 +5,17 @@ class agent extends uvm_agent;
   monitor   mon ;
   driver    drv ;
 
+  uvm_active_passive_enum is_active;
+
   function new(string name ="agent", uvm_component parent =null);
     super.new(name,parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(get_is_active()) begin
+    if(!uvm_config_db#(uvm_active_passive_enum)::get(this,"","is_active",is_active))
+      `uvm_info(get_type_name(),$sformatf("DID'nt get the agent_type"),UVM_LOW)
+    if(is_active == UVM_ACTIVE) begin
       seqr   = sequencer::type_id::create("sequencer",this);
       drv    = driver::type_id::create("drv",this);
     end
@@ -21,7 +25,7 @@ class agent extends uvm_agent;
 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    if(get_is_active())
+    if(is_active == UVM_ACTIVE)
       drv.seq_item_port.connect(seqr.seq_item_export);
   endfunction
 
